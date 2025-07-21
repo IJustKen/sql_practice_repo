ec4998af596732f2fe6592fdf691d3cc71b0e633
IJustKen <168433488+IJustKen@users.noreply.github.com>
2025-07-22 00:02:31 +0530

    Update rising_temperature.sql

--- basic_joins/rising_temperature.sql
+++ basic_joins/rising_temperature.sql
@@ -5,3 +5,9 @@ from weather today
 cross join weather yesterday    --this is like cartesian product so you get all possible date combos
 where today.recordDate - yesterday.recordDate = 1        --filter condition
 and today.temperature > yesterday.temperature;
+
+select today.id              --MUCH FASTER BECAUSE NO CROSS JOIN, cross join is more time consuming cuz you joining every possible val first and then filtering unlike with join on
+from weather today 
+join weather yesterday
+on yesterday.recordDate + interval '1 DAY' = today.recordDate
+and today.temperature > yesterday.temperature;
