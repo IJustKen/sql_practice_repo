a92ee948b662143d3c12d62fca2738955c79d126
IJustKen <168433488+IJustKen@users.noreply.github.com>
2026-08-16 12:31:48 +0530

    Update game_play_analysis_iv.sql

--- basic_aggregate_functions/game_play_analysis_iv.sql
+++ basic_aggregate_functions/game_play_analysis_iv.sql
@@ -1,3 +1,24 @@
+-- more optimized approach, almost same as the original approach but here
+-- while joining I joined on the second login = first login + 1 day condition
+-- this way we do not have unnecessary memory overhead during the left join which is more fast
+with first_logins as
+(
+    select 
+        player_id, 
+        min(event_date) as first_login 
+    from Activity
+    group by player_id
+)
+
+select 
+    round((count(a.player_id)::decimal/(select count(*) from first_logins)), 2) as fraction
+from first_logins f
+left join Activity a
+on f.player_id = a.player_id
+and f.first_login + interval '1 day' = a.event_date;    -- here we use INTERVAL '1 day' instead of directly integer 1
+
+    
+
 -- CTE to just get the first logins first
 with first_logins as
 (
