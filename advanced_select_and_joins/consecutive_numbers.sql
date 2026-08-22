a04a8696a8846a17736c46c2853641cce98e96f7
IJustKen <168433488+IJustKen@users.noreply.github.com>
2026-08-23 00:18:05 +0530

    Update consecutive_numbers.sql

--- advanced_select_and_joins/consecutive_numbers.sql
+++ advanced_select_and_joins/consecutive_numbers.sql
@@ -1,3 +1,19 @@
+-- faster method single pass, CTE + window function
+with chosen_rows as 
+(
+    select 
+        num,
+        lead(num, 1) over (order by id) as num_next,  -- this chooses the row 1 row after the current one
+        lead(num, 2) over (order by id) as num_nnext  -- ordering by id makes sure these nums are consecutive even if there are gaps between id
+    from Logs
+)
+-- made CTE because lead() as num_next gets evaluated AFTER the where keyword, so num_next n all do not exist to filter anything 
+select distinct num as ConsecutiveNums
+from chosen_rows
+where num = num_next and num = num_nnext;  -- condition to get 3 consecutive
+
+
+-- sequential joining method
 select distinct t1.num as ConsecutiveNums  -- avoid dupes
 from Logs t1
 inner join Logs t2
